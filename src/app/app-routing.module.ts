4c3928e3c8175e1b0d223012f2e302fe38e3ed86
Hornshade <bejinari.sabin@gmail.com>
2022-08-18 15:10:12 +0300

    single page finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardsComponent } from './pages/cards/cards.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SingleComponent } from './pages/single/single.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Home page' },
-  { path: 'category/:categ', component: CardsComponent, title: 'Category' },
-  { path: 'listing/:id', component: CardsComponent, title: 'Single page' },
-  { path: '**', redirectTo: '' },
+	{ path: '', component: HomeComponent, title: 'Home page' },
+	{ path: 'category/:categ', component: CardsComponent, title: 'Category' },
+	{ path: 'listing/:id', component: SingleComponent, title: 'Single page' },
+	{ path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/single/single.component.ts
+++ src/app/pages/single/single.component.ts
@@ -2,25 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Listing } from '../../interfaces/listing';
 import { ListingService } from 'src/app/services/listings/listing.service';
+import { MatDialog } from '@angular/material/dialog';
+import { GaleryModalComponent } from 'src/app/components/modal/galery-modal/galery-modal.component';
+import { FavoriteModalComponent } from 'src/app/components/modal/favorite-modal/favorite-modal.component';
 
 @Component({
-  selector: 'app-single',
-  templateUrl: './single.component.html',
-  styleUrls: ['./single.component.scss'],
+	selector: 'app-single',
+	templateUrl: './single.component.html',
+	styleUrls: ['./single.component.scss'],
 })
 export class SingleComponent implements OnInit {
-  listing!: Listing[];
-  urlId!: string | null;
+	listing = {} as Listing;
+	urlId!: string | null;
 
-  constructor(
-    private route: ActivatedRoute,
-    private listingsService: ListingService
-  ) {}
+	constructor(
+		private route: ActivatedRoute,
+		private listingsService: ListingService,
+		public dialog: MatDialog
+	) {}
 
-  ngOnInit(): void {
-    this.urlId = this.route.snapshot.paramMap.get('id');
-    this.listingsService
-      .getListingById(this.urlId)
-      .subscribe((data) => (this.listing = data));
-  }
+	ngOnInit(): void {
+		this.urlId = this.route.snapshot.paramMap.get('id');
+		console.log(this.urlId);
+		this.listingsService
+			.getListingById(this.urlId)
+			.subscribe((data) => (this.listing = data));
+	}
+
+	openDialog() {
+		const dialogRef = this.dialog.open(GaleryModalComponent, {
+			height: '100%',
+			width: '100%',
+		});
+
+		dialogRef.afterClosed().subscribe((result) => {
+			console.log(`Dialog result: ${result}`);
+		});
+	}
+
+	openFavoriteDialog() {
+		const dialogRef = this.dialog.open(FavoriteModalComponent);
+
+		dialogRef.afterClosed().subscribe((result) => {
+			console.log(`Dialog result: ${result}`);
+		});
+	}
+
+	scrollToBottom() {
+		window.scrollTo(0, document.body.scrollHeight);
+	}
+
+	addToFavorite() {
+		// if user logged in add to favorite
+		// else
+		this.openFavoriteDialog();
+	}
 }
