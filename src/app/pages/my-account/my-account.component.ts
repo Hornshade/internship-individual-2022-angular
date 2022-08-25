1e8a1e288f5e6cdcc2fee105a3fe122bb1d1c805
Hornshade <bejinari.sabin@gmail.com>
2022-08-25 10:30:19 +0300

    tabs functionality

--- src/app/pages/my-account/my-account.component.ts
+++ src/app/pages/my-account/my-account.component.ts
@@ -7,6 +7,10 @@ import { Router } from '@angular/router';
 	styleUrls: ['./my-account.component.scss'],
 })
 export class MyAccountComponent implements OnInit {
+	profile: boolean = true;
+	security: boolean = false;
+	notifications: boolean = false;
+	chat: boolean = false;
 	constructor(private router: Router) {}
 
 	ngOnInit(): void {}
@@ -16,5 +20,34 @@ export class MyAccountComponent implements OnInit {
 		this.router.navigate(['']).then(() => {
 			window.location.reload();
 		});
+	}
+
+	toggle(content: string) {
+		switch (content) {
+			case 'profile':
+				this.profile = true;
+				this.security = false;
+				this.notifications = false;
+				this.chat = false;
+				break;
+			case 'security':
+				this.profile = false;
+				this.security = true;
+				this.notifications = false;
+				this.chat = false;
+				break;
+			case 'notification':
+				this.profile = false;
+				this.security = false;
+				this.notifications = true;
+				this.chat = false;
+				break;
+			case 'chat':
+				this.profile = false;
+				this.security = false;
+				this.notifications = false;
+				this.chat = true;
+				break;
+		}
 	}
 }
