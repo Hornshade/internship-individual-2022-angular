145d3526d3282f2ea379f9530caa06a476a8d99c
Hornshade <bejinari.sabin@gmail.com>
2022-08-31 16:06:04 +0300

    singlepage fix favorites and author

--- src/app/pages/single/single.component.ts
+++ src/app/pages/single/single.component.ts
@@ -5,6 +5,8 @@ import { ListingService } from 'src/app/services/listings/listing.service';
 import { MatDialog } from '@angular/material/dialog';
 import { GaleryModalComponent } from 'src/app/components/modal/galery-modal/galery-modal.component';
 import { FavoriteModalComponent } from 'src/app/components/modal/favorite-modal/favorite-modal.component';
+import { FavoriteService } from 'src/app/services/favorite/favorite.service';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
 	selector: 'app-single',
@@ -14,18 +16,34 @@ import { FavoriteModalComponent } from 'src/app/components/modal/favorite-modal/
 export class SingleComponent implements OnInit {
 	listing = {} as Listing;
 	urlId!: string | null;
+	favorites: Listing[] = [];
+
+	isLogged: boolean = false;
+	favorite: boolean = false;
+	userId!: string | null;
 
 	constructor(
 		private route: ActivatedRoute,
 		private listingsService: ListingService,
+		private loginService: LoginService,
+		private favoriteService: FavoriteService,
 		public dialog: MatDialog
-	) {}
+	) {
+		this.userId = localStorage.getItem('userId');
+	}
 
 	ngOnInit(): void {
+		this.loginService.isLoggedIn.subscribe((data) => (this.isLogged = data));
 		this.urlId = this.route.snapshot.paramMap.get('id');
 		this.listingsService
 			.getListingById(this.urlId)
 			.subscribe((data) => (this.listing = data));
+		if (this.isLogged) {
+			if (this.userId !== null)
+				this.favoriteService.getFavorite(this.userId).subscribe((data) => {
+					this.favorites = data;
+				});
+		}
 	}
 
 	openDialog() {
@@ -56,8 +74,27 @@ export class SingleComponent implements OnInit {
 	}
 
 	addToFavorite() {
-		// if user logged in add to favorite
-		// else
-		this.openFavoriteDialog();
+		if (this.isLogged) {
+			if (this.favorite === true) {
+				this.favoriteService
+					.deleteFavorite(localStorage.getItem('userId'), this.listing.id)
+					.subscribe();
+				this.favorite = false;
+			} else if (this.favorite === false) {
+				this.favoriteService
+					.addToFavorites(localStorage.getItem('userId'), this.listing.id)
+					.subscribe();
+				this.favorite = true;
+			}
+		} else {
+			this.openFavoriteDialog();
+		}
+	}
+	getFavStatus(id: string) {
+		return this.favorites.findIndex((fav) => {
+			return fav.id === id;
+		}) !== -1
+			? true
+			: false;
 	}
 }
